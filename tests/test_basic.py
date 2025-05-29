8e2e14c9b8c5a2b728ffaa3b54e99c6113f6ca73
juzername2912 <iginacjo@gmail.com>
2025-05-29 14:39:38 +0200

    Update test_basic.py
    test filtra 2D

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -14,9 +14,23 @@ def test_add():
 def test_sub():
     assert m.subtract(1, 2) == -1
 
-def test_filtr():
-    print(_core.apply_filter([0, 0, 1, 1, 0, 0], [0.25, 0.5, 0.25]))
+def test_filtr1D():
+    print(m.apply_filter([0, 0, 1, 1, 0, 0], [0.25, 0.5, 0.25]))
 
+def test_filtr2D():
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    kernel = [
+        [0, -1, 0],
+        [-1, 5, -1],
+        [0, -1, 0]
+    ]
+    result = m.apply_filter_2D(matrix, kernel)
+    for row in result:
+        print(row)
 
 
 def main():
